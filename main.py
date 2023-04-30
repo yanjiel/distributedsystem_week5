817e8d763feadacca32aca89b0767afd3dd66cfd
yanjiel <yanjieliu829@gmail.com>
2023-04-30 00:24:57 -0500

    2nd commit execute function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import uvicorn
 from models import *
 import json
 import redis
+import types
+
 
 app = FastAPI()
 r = redis.Redis()
@@ -18,7 +20,7 @@ def register(func: RegisterFn) -> RegisterFnRep:
         func_id = str(uuid.uuid4())
 
     # save into redis database
-    r.set(func_id, json.dumps({func.name:func.payload})) #"f1c33bf6-71d2-4d2c-9419-36a21de86d0d", b'{"func1": "gASVFgAAAAAAAACMCF9fbWFpbl9flIwFZnVuYzGUk5Qu\\n"}'
+    r.set(func_id, json.dumps([func.name, func.payload])) #"f1c33bf6-71d2-4d2c-9419-36a21de86d0d", b'{"func1": "gASVFgAAAAAAAACMCF9fbWFpbl9flIwFZnVuYzGUk5Qu\\n"}'
 
     # print status
     print(f'Adding {func.name} to database! function_id: {func_id}')
@@ -37,20 +39,33 @@ async def register_function(func: RegisterFn) -> RegisterFnRep:
 
 def execute(func_w_param: ExecuteFnReq) -> ExecuteFnRep:
     func_id = str(func_w_param.function_id)
-    param = str(func_w_param.payload)
-    func_info = r.get(func_id)
-    
-    func_compiled = compile(deserialize(func_info.payload), func_info.name, "exec")
-    exec(param) ######################################
-    exec(func_compiled) ##############################
+    func_info = json.loads(r.get(func_id))
+    print(type(func_info), func_info)
 
+    func_name = func_info[0]
+    func_body = deserialize(func_info[1]) #function object
+    param = deserialize(func_w_param.payload) #[1,2]
+
+    print(type(func_name), func_name)
+    print(type(func_body), func_body)
+    print(type(param), param)
+    
+    if isinstance(func_body, types.FunctionType):
+        func_body(*param)
+    elif isinstance(func_body, str):
+        func_compiled = compile(func_body, func_name, "exec")
+        exec(param)
+        exec(func_compiled)
+    else:
+        raise RuntimeError("Deserialized function is of the wrong type.")
+    
     task_id = str(uuid.uuid4())
     while task_id in r.keys():
         task_id = str(uuid.uuid4())
     return {"task_id": uuid.UUID(task_id)}
 
 
-@app.get('/execute_fuction/')
+@app.post('/execute_fuction/')
 async def execute_function(func_w_param: ExecuteFnReq) -> ExecuteFnRep:
     return execute(func_w_param)
 
